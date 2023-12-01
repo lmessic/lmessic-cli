539e6562aa349b6f68dc1c867e10f526c77d0bab
lmessic <1296601488@qq.com>
2023-12-01 15:40:44 +0800

    chore: 删除多余文件；

--- my-demo/postcss.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	ident: 'postcss',
-	// eslint-disable-next-line global-require
-	plugins: [require('autoprefixer')]
-};
--- my-demo/src/components/index.tsx
+++
@@ -1,3 +0,0 @@
-// src/components/index.ts
-export { default as Demo1 } from './demo1';
-export { default as Demo2 } from './demo2';
